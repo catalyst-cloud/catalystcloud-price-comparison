949bfe013b3f9de663d2a26639b0798ebda76956
David Daish <david@daish.me>
2018-07-17 09:09:55 +1200

    Changed label to n vCPUs, m GB RAM instead of CCloud flavour names

--- csv_to_graph.py
+++ csv_to_graph.py
@@ -12,21 +12,44 @@ graph_template = env.get_template('graph.js')
 # Import cloud price data
 df = pd.read_csv('predicted-dataset/predicted_catalyst_prices.csv', index_col=0)
 
-names_list = list(df['Name'])
+# Define a function that removes empty decimal places from numbers ('16.0' to
+# '16') to shorten the labels on the graph without losing any detail.
+def shorten_num(float):
+    if float.is_integer():
+        return str(int(float))
+    else:
+        return str(float)
+
+# Extract labels from the csv, and format them as a string we can insert into
+# the template.
+names_list = []
+name_string = "{cpu} vCPUs, {ram} GB RAM"
+for index, row in df.iterrows():
+
+    short_cpu = shorten_num(row['vCPU'])
+    short_ram = shorten_num(row['RAM, GB'])
+
+    formatted_name_string = name_string.format(cpu=short_cpu, ram=short_ram)
+
+    names_list.append(formatted_name_string)
 names_list = ['"' + x + '"' for x in names_list]
 names_list = ', '.join(names_list)
 
+# Define fucntion that extracts prices from the csv and formats them as a string
+# we can insert into the template.
 def get_price_string(label):
     prices = list(df[label])
     prices = [format(x, '.3f') for x in prices]
     prices = ', '.join(prices)
     return prices
 
+# Extract price data.
 catalyst_data = get_price_string('Catalyst price per hour, NZD (ex GST)')
 google_data = get_price_string('Google price per hour, NZD (ex GST)')
 aws_data = get_price_string('AWS price per hour, NZD (ex GST)')
 azure_data = get_price_string('Azure price per hour, NZD (ex GST)')
 
+# Render the template with the labels and price data.
 data_filled_js = graph_template.render(
     labels=names_list,
     catalyst_data=catalyst_data,
@@ -35,5 +58,9 @@ data_filled_js = graph_template.render(
     azure_data=azure_data
 )
 
+# Write the rendered template into a file for use.
 with open('display/js/graph.js', 'w') as graph_file:
     graph_file.write(data_filled_js)
+
+# The ./display directory now has an updated price comparison graph that can be
+# inserted as an iframe html tag.
